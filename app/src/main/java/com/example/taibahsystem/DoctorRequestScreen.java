c528ec23926ab9808181e0f033c185900e4cb996
Abdul-Rahmman <kudabaksh44@gmail.com>
2017-03-21 07:34:36 +0300

    Doctor Activity

--- app/src/main/java/com/example/taibahsystem/DoctorRequestScreen.java
+++ app/src/main/java/com/example/taibahsystem/DoctorRequestScreen.java
@@ -10,5 +10,6 @@ public class DoctorRequestScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_request_screen);
+
     }
 }
