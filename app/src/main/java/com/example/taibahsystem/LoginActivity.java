1b57a19bf8602559acde7acadf74ca07c58ee821
qshaden <im3gax@gmail.com>
2017-03-23 10:25:41 +0300

    Login in Act

--- app/src/main/java/com/example/taibahsystem/LoginActivity.java
+++ app/src/main/java/com/example/taibahsystem/LoginActivity.java
@@ -57,7 +57,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private UserLoginTask mAuthTask = null;
 
     // UI references.
-    private AutoCompleteTextView mEmailView;
+    private AutoCompleteTextView mUsernameView;
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
@@ -67,7 +67,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        mUsernameView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
@@ -110,7 +110,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             return true;
         }
         if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
+            Snackbar.make(mUsernameView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
                     .setAction(android.R.string.ok, new View.OnClickListener() {
                         @Override
                         @TargetApi(Build.VERSION_CODES.M)
@@ -149,11 +149,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
 
         // Reset errors.
-        mEmailView.setError(null);
+        mUsernameView.setError(null);
         mPasswordView.setError(null);
 
         // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
+        String email = mUsernameView.getText().toString();
         String password = mPasswordView.getText().toString();
 
         boolean cancel = false;
@@ -168,12 +168,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         // Check for a valid email address.
         if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
+            mUsernameView.setError(getString(R.string.error_field_required));
+            focusView = mUsernameView;
             cancel = true;
         } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
+            mUsernameView.setError(getString(R.string.error_invalid_email));
+            focusView = mUsernameView;
             cancel = true;
         }
 
@@ -276,7 +276,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 new ArrayAdapter<>(LoginActivity.this,
                         android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
 
-        mEmailView.setAdapter(adapter);
+        mUsernameView.setAdapter(adapter);
     }
 
 
--- app/src/main/java/com/example/taibahsystem/MainActivity.java
+++ app/src/main/java/com/example/taibahsystem/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.taibahsystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Call;
+
 import com.example.taibahsystem.Classes.*;
+
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,8 +17,86 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+/*
+    public interface ConnectionUtilityAPI{
+
+        @GET("getrequest.php")
+        Call<ResultModel> getRequests();
+
+    }
+
+    // Building Retrofit and get connection
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("")
+            .addConverterFactory(GsonConverter.create())
+            .build();
+
+    // Link retrofit into API
+    ConnectionUtilityAPI cuApi = retrofit.create(ConnectionUtilityAPI.class);
+
+    // Calling the method getRequest() and catch Call name of connection
+    Call<ResultModel> connection = cuApi.getRequests();
+
+    connection.enqueue()
 
 
-        //test test test
+
+    // Mapping for Json
+    public class ResultModel{
+
+        private List<Request> requests;
+
+        public List<Request> getRequests(){
+            return requests;
+        }
+
+        public void setRequests(List<Request> requests){
+            this.requests = requests;
+        }
+    }
+*/
+
+    // Model for the data. In this case it's General Data Model for the Requests
+    public class Request{
+
+        int number;
+        int studentID;
+        String type;
+        String status;
+
+        public int getNumber(){
+            return number;
+        }
+
+        public void setNumber(int number){
+            this.number = number;
+        }
+
+        public int getStudentID(){
+            return studentID;
+        }
+
+        public void setStudentID(int studentID){
+            this.studentID = studentID;
+        }
+
+        public String getType(){
+            return type;
+        }
+
+        public void setType(){
+            this.type = type;
+        }
+
+        public String getStatus(){
+            return status;
+        }
+
+        public void setStatus(){
+            this.status = status;
+        }
+
     }
 }
